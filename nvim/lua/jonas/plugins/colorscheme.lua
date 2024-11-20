e77da774ea1ffc65be7a758b2df788b15fcc8d8c
Jonas1145 <u27e51@DERGBWN111133.avl01.avlcorp.lan>
2024-11-20 09:14:04 +0100

    add black mode

--- nvim/lua/jonas/plugins/colorscheme.lua
+++ nvim/lua/jonas/plugins/colorscheme.lua
@@ -3,41 +3,73 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-      local transparent = false -- set to true if you would like to enable transparency
-      local bg = "#011628"
-      local bg_dark = "#011423"
-      local bg_highlight = "#143652"
-      local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
-
+      -- Store the default TokyoNight configuration
       require("tokyonight").setup({
-        transparent = transparent,
+        transparent = false,
         styles = {
-          sidebars = transparent and "transparent" or "dark",
-          floats = transparent and "transparent" or "dark",
+          sidebars = "dark",
+          floats = "dark",
         },
-        -- on_colors = function(colors)
-        --   colors.bg = bg
-        --   colors.bg_dark = transparent and colors.none or bg_dark
-        --   colors.bg_float = transparent and colors.none or bg_dark
-        --   colors.bg_highlight = bg_highlight
-        --   colors.bg_popup = bg_dark
-        --   colors.bg_search = bg_search
-        --   colors.bg_sidebar = transparent and colors.none or bg_dark
-        --   colors.bg_statusline = transparent and colors.none or bg_dark
-        --   colors.bg_visual = bg_visual
-        --   colors.border = border
-        --   colors.fg = fg
-        --   colors.fg_dark = fg_dark
-        --   colors.fg_float = fg
-        --   colors.fg_gutter = fg_gutter
-        --   colors.fg_sidebar = fg_dark
-        -- end,
       })
+
+      -- Create a command to switch to black version
+      local function set_black_theme()
+        local bg = "#000000" -- Pure black background
+        local bg_dark = "#000000"
+        local bg_highlight = "#1a1a1a"
+        local bg_search = "#2a2a2a"
+        local bg_visual = "#202020"
+        local fg = "#cdd6f4"
+        local fg_dark = "#bac2de"
+        local fg_gutter = "#45475a"
+        local border = "#313244"
+
+        require("tokyonight").setup({
+          transparent = false,
+          styles = {
+            sidebars = "dark",
+            floats = "dark",
+          },
+          on_colors = function(colors)
+            colors.bg = bg
+            colors.bg_dark = bg_dark
+            colors.bg_float = bg_dark
+            colors.bg_highlight = bg_highlight
+            colors.bg_popup = bg_dark
+            colors.bg_search = bg_search
+            colors.bg_sidebar = bg_dark
+            colors.bg_statusline = bg_dark
+            colors.bg_visual = bg_visual
+            colors.border = border
+            colors.fg = fg
+            colors.fg_dark = fg_dark
+            colors.fg_float = fg
+            colors.fg_gutter = fg_gutter
+            colors.fg_sidebar = fg_dark
+          end,
+        })
+        vim.cmd([[colorscheme tokyonight]])
+      end
+
+      -- Create commands to switch between themes
+      vim.api.nvim_create_user_command('TokyoNightBlack', function()
+        set_black_theme()
+      end, {})
+
+      vim.api.nvim_create_user_command('TokyoNightDefault', function()
+        require("tokyonight").setup({
+          transparent = false,
+          styles = {
+            sidebars = "dark",
+            floats = "dark",
+          },
+        })
+        vim.cmd([[colorscheme tokyonight]])
+      end, {})
+
+      -- Optional: Set up keybindings
+      vim.keymap.set('n', '<leader>tb', ':TokyoNightBlack<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>td', ':TokyoNightDefault<CR>', { noremap = true, silent = true })
     end,
   },
   {
@@ -68,4 +100,3 @@ return {
     end,
   },
 }
-
